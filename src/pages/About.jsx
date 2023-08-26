596dc0cc7c1a5a86f479f3864e91ea881991cff5
tharaka-19052 <tharaka-se19052@stu.kln.ac.lk>
2023-08-26 21:31:26 +0530

    feat: modified about component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,6 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer';
 import '../styles/about.css'
 
-
 function About() {
   return (   
     <div className='about-container'>
@@ -16,75 +15,85 @@ function About() {
         <div className="row about-container1 nav">
         <Navbar/>
         </div>
-      
-    <div id="carouselExampleDark" className="carousel carousel-dark slide hello col-5 left">
-      <div className="carousel-indicators">
-        <button
-          type="button"
-          data-bs-target="#carouselExampleDark"
-          data-bs-slide-to="0"
-          className="active"
-          aria-current="true"
-          aria-label="Slide 1"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#carouselExampleDark"
-          data-bs-slide-to="1"
-          aria-label="Slide 2"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#carouselExampleDark"
-          data-bs-slide-to="2"
-          aria-label="Slide 3"
-        ></button>
+        <div className="row about-content-all">
+      <div className=" col-5 slider ">
+        <div id="carouselExampleDark" className="carousel carousel-dark slide hello left" data-bs-ride="carousel">
+  <div className="carousel-indicators">
+    <button
+      type="button"
+      data-bs-target="#carouselExampleDark"
+      data-bs-slide-to="0"
+      className="active"
+      aria-current="true"
+      aria-label="Slide 1"
+    ></button>
+    <button
+      type="button"
+      data-bs-target="#carouselExampleDark"
+      data-bs-slide-to="1"
+      aria-label="Slide 2"
+    ></button>
+    <button
+      type="button"
+      data-bs-target="#carouselExampleDark"
+      data-bs-slide-to="2"
+      aria-label="Slide 3"
+    ></button>
+  </div>
+  <div className="carousel-inner ">
+    <div className="carousel-item active" data-bs-interval="3000"> 
+      <img src={img1} className="d-block w-100" alt="..." />
+      <div className="carousel-caption d-none d-md-block slide-content">
+        <h5 className='heading'>Welcome to</h5>
+        <p className='para'>Sigiriya</p>
       </div>
-      <div className="carousel-inner">
-        <div className="carousel-item active" data-bs-interval="10000">
-          <img src={img1} className="d-block w-100" alt="..." />
-          <div className="carousel-caption d-none d-md-block slide-content">
-            <h5 className='heading'>Welcome to</h5>
-            <p className='para'>Sigiriya</p>
-          </div>
-        </div>
-        <div className="carousel-item" data-bs-interval="2000">
-          <img src={img2} className="d-block w-100" alt="..." />
-          <div className="carousel-caption d-none d-md-block slide-content">
-            <h5 className='heading' >kkk</h5>
-            <p className='para'>ooo</p>
-          </div>
-        </div>
-        <div className="carousel-item">
-          <img src={img3} className="d-block w-100" alt="..." />
-          <div className="carousel-caption d-none d-md-block slide-content">
-            <h5 className='heading'>jnkj</h5>
-            <p className='para'>klk</p>
-          </div>
-        </div>
+    </div>
+    <div className="carousel-item" data-bs-interval="1500"> 
+      <img src={img2} className="d-block w-100" alt="..." />
+      <div className="carousel-caption d-none d-md-block slide-content">
+        <h5 className='heading' >kkk</h5>
+        <p className='para'>ooo</p>
       </div>
-      <button
-        className="carousel-control-prev prev-icon"
-        type="button"
-        data-bs-target="#carouselExampleDark"
-        data-bs-slide="prev"
-      >
-        <span className="carousel-control-prev-icon " aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleDark"
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
+    </div>
+    <div className="carousel-item" data-bs-interval="2000"> 
+      <img src={img3} className="d-block w-100" alt="..." />
+      <div className="carousel-caption d-none d-md-block slide-content">
+        <h5 className='heading'>jnkj</h5>
+        <p className='para'>klk</p>
+      </div>
+    </div>
   </div>
-  <Footer/>
-  </div>
-  </div>
+  <button
+    className="carousel-control-prev prev-icon"
+    type="button"
+    data-bs-target="#carouselExampleDark"
+    data-bs-slide="prev"
+  >
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button
+    className="carousel-control-next"
+    type="button"
+    data-bs-target="#carouselExampleDark"
+    data-bs-slide="next"
+  >
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
+<div className="col-7 paragraph">
+  <p className='about-content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+</div>
+<Footer/>
+</div>
+</div>
   )
 }
 
