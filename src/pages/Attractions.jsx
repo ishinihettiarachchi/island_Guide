2b7f77fe76968a0a0d29202d85525faeb4135313
tharaka-19052 <tharaka-se19052@stu.kln.ac.lk>
2023-08-27 18:51:25 +0530

    feat: added pagination

--- src/pages/Attractions.jsx
+++ src/pages/Attractions.jsx
@@ -4,10 +4,35 @@ import '../styles/attractions.css'
 import Footer from '../components/Footer'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import ReactPaginate from 'react-paginate';
 
 
 function Attractions() {
   const [places, setPlaces] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const placesPerPage = 8;
+  const pagesVisited = pageNumber * placesPerPage;
+
+  const displayPlaces = places.slice(pagesVisited, pagesVisited+placesPerPage)
+  .map(places => {
+    return  <div class="col-lg-3 mb-3">
+          <div class="card place">
+            <img src={places.image} alt="" class="card-img-top"/>
+            <div class="card-body">
+              <h5 class="card-title">{places.name}</h5>
+              <p class="card-text">{places.description}</p>
+             <a href="" class="btn btn-outline-success btn-sm">Read More</a>
+            </div>
+           </div>
+          </div>
+  });
+
+  const pageCount = Math.ceil(places.length/placesPerPage)
+ 
+  const changePage = ({selected}) => {
+      setPageNumber(selected)
+  }
 
   useEffect(()=>{
     axios.get('http://localhost:3001/api/v1/places')
@@ -50,20 +75,24 @@ function Attractions() {
   <div class="container">
     <div class="row">
       {
-        places.map(places=>{
-          return  <div class="col-lg-3 mb-3">
-          <div class="card place">
-            <img src={places.image} alt="" class="card-img-top"/>
-            <div class="card-body">
-              <h5 class="card-title">{places.name}</h5>
-              <p class="card-text">{places.description}</p>
-             <a href="" class="btn btn-outline-success btn-sm">Read More</a>
-            </div>
-           </div>
-          </div>
-        })
+        displayPlaces
       }
-    
+      <div className="paging">
+        <ReactPaginate 
+        
+            previousLabel={"Previous"}
+            nextLabel={"Next"}
+            pageCount={pageCount}
+            onPageChange={changePage}
+            containerClassName={"paginationBttns"}
+            previousLinkClassName={ "previousBtton"}
+            nextLinkClassName={"nextBttn"}
+            disabledClassName={"paginationDisabled"}
+            activeClassName={"paginationActive"}
+            previousClassName={pageNumber === 0 ? "hidden" : ""}
+        />
+     
+     </div>
 
   </div>
 </div>
