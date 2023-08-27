4023bc8c1d6ae3488f3e7773d0b6585038c0a991
tharaka-19052 <tharaka-se19052@stu.kln.ac.lk>
2023-08-27 17:51:01 +0530

    feat: updated attractions component

--- src/pages/Attractions.jsx
+++ src/pages/Attractions.jsx
@@ -68,52 +68,7 @@ function Attractions() {
   </div>
 </div>
 </section>
-<section id="gallery">
-  <div class="container">
-    <div class="row">
-    <div class="col-lg-3 mb-3">
-    <div class="card place">
-      <img src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt="" class="card-img-top"/>
-      <div class="card-body">
-        <h5 class="card-title">Sunset</h5>
-        <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit.  Incidunt repellendus praesentium quae!</p>
-       <a href="" class="btn btn-outline-success btn-sm">Read More</a>
-      </div>
-     </div>
-    </div>
-    <div class="col-lg-3 mb-3">
-    <div class="card place">
-      <img src="https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt="" class="card-img-top"/>
-      <div class="card-body">
-        <h5 class="card-title">Sunset</h5>
-        <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit.  Incidunt repellendus praesentium quae!</p>
-       <a href="" class="btn btn-outline-success btn-sm">Read More</a>
-      </div>
-     </div>
-    </div>
-  <div class="col-lg-3 mb-3">
-  <div class="card">
-      <img src="https://images.unsplash.com/photo-1516214104703-d870798883c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt="" class="card-img-top"/>
-      <div class="card-body">
-        <h5 class="card-title">Sunset</h5>
-        <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit.  Incidunt repellendus praesentium quae!</p>
-       <a href="" class="btn btn-outline-success btn-sm">Read More</a>
-      </div>
-      </div>
-    </div>
-    <div class="col-lg-3 mb-3">
-  <div class="card">
-      <img src="https://images.unsplash.com/photo-1516214104703-d870798883c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60" alt="" class="card-img-top"/>
-      <div class="card-body">
-        <h5 class="card-title">Sunset</h5>
-        <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit.  Incidunt repellendus praesentium quae!</p>
-       <a href="" class="btn btn-outline-success btn-sm">Read More</a>
-      </div>
-      </div>
-    </div>
-  </div>
-</div>
-</section>
+
 
           </div>
         </div>
