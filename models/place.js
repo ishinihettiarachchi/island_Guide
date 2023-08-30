cebcb984ec56988737c8b366b9bef678e82aef44
tharaka-19052 <tharaka-se19052@stu.kln.ac.lk>
2023-08-30 13:50:43 +0530

    feat: modified model

--- models/place.js
+++ models/place.js
@@ -5,6 +5,12 @@ const placeSchema = mongoose.Schema({
     name: String,
     image: String,
     description: String,
+    images:{
+        image1:String,
+        image2:String,
+        image3:String,
+        image4:String,
+    }
 })
 
 
